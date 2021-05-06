0d2fd5a1475ad92ee4c9e7d2f7273850e6dc4ac4
realchanin <6110110589@psu.ac.th>
2021-05-07 02:25:17 +0700

    form vadilate and delete button with confirm alert

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,10 +1,52 @@
 var express = require('express');
 var router = express.Router();
+const Blogs = require('../models/blogs');
+const { check, validationResult } = require('express-validator');
 
-router.get('/', function(req, res, next) {
-    res.render("blogs/index", {data:"ข้อมูลบทความ"});
-  });
-router.get('/add', function(req, res, next) {
-    res.render("blogs/addForm", {data:"เพิ่มบทความ"});
+router.get('/', function (req, res, next) {
+    Blogs.getAllBlogs(function (err, blogs) {
+        if (err) throw err
+        res.render("blogs/index", { data: "ข้อมูลบทความ", blogs: blogs });
+    });
 });
+router.get('/add', function (req, res, next) {
+    res.render("blogs/addForm", { data: "เพิ่มบทความ" });
+});
+router.get('/delete/:id', function (req, res, next) {
+    console.log("FIND ID",req.params.id);
+    Blogs.deleteBlog([req.params.id],function(err){
+        if(err) throw err
+        console.log("DELETE COMPLETE");
+        res.redirect("/blogs")
+    })
+});
+router.post('/add', [
+    check('title', 'กรุณาป้อนชื่อบทความ').not().isEmpty(),
+    check('author', 'กรุณาป้อนชื่อผู้แต่ง').not().isEmpty(),
+], function (req, res, next) {
+    const result = validationResult(req);
+    var errors = result.errors;
+    for (var key in errors) {
+        console.log(errors[key].value);
+    }
+    if (!result.isEmpty()) {
+        //response validate data to register.ejs
+        res.render("blogs/addForm", { data: "เพิ่มบทความ", errors: errors });
+    } else {
+        var data = new Blogs({
+            title: req.body.title,
+            author: req.body.author,
+            category: req.body.category
+        })
+        console.log(req.body.title);
+        console.log(req.body.author);
+        console.log(req.body.category);
+        Blogs.createblog(data, function (err, callback) {
+            if (err) console.log(err);
+            res.redirect("/blogs")
+        })
+    }
+
+});
+
 module.exports = router;
