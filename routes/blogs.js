7e1faabc7f0e75fe46e87303f758a2ab8904fe0b
realchanin <6110110589@psu.ac.th>
2021-05-06 22:26:16 +0700

    add form

---
+++ routes/blogs.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+    res.render("blogs/index", {data:"ข้อมูลบทความ"});
+  });
+router.get('/add', function(req, res, next) {
+    res.render("blogs/addForm", {data:"เพิ่มบทความ"});
+});
+module.exports = router;
